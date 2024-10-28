d7e232a67e016e59d6afdd58d3fa77f2a21433de
Vinct-852 <cchunyungg123@gmail.com>
2024-10-28 16:15:24 +0800

    [ADD] base functions for vesc_motor class

--- vesc_motor/vesc_motor.cpp
+++ vesc_motor/vesc_motor.cpp
@@ -2,15 +2,21 @@
 
 vesc_motor::vesc_motor(CAN *can, int can_id)
 {
-    this->motor_driver = vesc_control(can, can_id);
+    this->motor_driver = new vesc_control(can, can_id);
     this->can_id = can_id;
 }
 
 vesc_motor::~vesc_motor()
 {
+    delete this->motor_driver;
 }
 
 void vesc_motor::set_rpm(float rpm)
 {
     this->motor_driver->set_rpm(rpm);
+}
+
+void vesc_motor::set_pos(float pos)
+{
+    this->motor_driver->set_pos(pos);
 }
--- vesc_motor/vesc_motor.h
+++ vesc_motor/vesc_motor.h
@@ -6,6 +6,13 @@ class vesc_motor {
     vesc_motor(CAN* can, int can_id);
     ~vesc_motor();
 
+    //based functions
+    void set_rpm(float rpm);
+    void set_pos(float pos);
+
+    //customized functions
+    
+
     private:
     int can_id;
     vesc_control* motor_driver;
